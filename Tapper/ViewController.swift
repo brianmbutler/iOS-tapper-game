928f99eeb56e9e57cb06758998374e89588c9460
Brian Butler <brian_m_butler@icloud.com>
2015-11-28 14:24:36 -0500

    Initial commit

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -10,16 +10,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Properties
+    
+    var maxTaps = 0
+    var currentTaps = 0
+   
+    //Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLabel: UILabel!
+    
+    
+    @IBAction func onPlayButtonPressed(sender: UIButton) {
+        
+        if howManyTxt.text != nil && howManyTxt.text != "" {
+            
+        logoImg.hidden = true
+        howManyTxt.hidden = true
+        playBtn.hidden = true
+        
+        tapBtn.hidden = false
+        tapsLabel.hidden = false
+       
+        maxTaps = Int(howManyTxt.text!)!
+        currentTaps = 0
+        
+        updateTapsLbl()
+        
+    }
+    }
+    
+    @IBAction func tapBtnTapped(sender: UIButton!) {
+        currentTaps++
+        updateTapsLbl()
+        print("\(currentTaps)")
+        
+        if isGameOver() {
+            restartGame()
+        }
+    }
+    
+
+    
+    func restartGame() {
+        maxTaps = 0
+        howManyTxt.text = ""
+        
+        logoImg.hidden = false
+        howManyTxt.hidden = false
+        playBtn.hidden = false
+        
+        tapBtn.hidden = true
+        tapsLabel.hidden = true
+    }
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+        return false
+    }
+    }
+    
+    func updateTapsLbl() {
+        tapsLabel.text = "\(currentTaps) taps"
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
-
-}
+    
+    
+    
+    
 
